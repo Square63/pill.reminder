b305e69eb38f5a6acd113712cb2438efe24f7c22
Ahmed Raza <ahmed.raza@square63.com>
2023-07-04 13:22:38 +0500

    Add crisy bootstrap and form apps to settings with their configs

--- pill_reminder/settings.py
+++ pill_reminder/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pillreminder.apps.PillreminderConfig'
+    'pillreminder.apps.PillreminderConfig',
+    'crispy_forms',
+    'crispy_bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
