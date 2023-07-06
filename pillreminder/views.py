20760df936a90d421e8ad8799cc0563ce5f1f120
Ahmed Raza <ahmed.raza@square63.com>
2023-07-06 13:10:57 +0500

    Class based SignUp view definition

--- pillreminder/views.py
+++ pillreminder/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
     return render(request, 'pillreminder/home.html')
+
+class SignUp(CreateView):
+    model = User
+    template_name = 'pillreminder/signup.html'
+    success_url = reverse_lazy("login")
+    form_class = SignUpForm
