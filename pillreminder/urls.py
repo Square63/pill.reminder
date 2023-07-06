09e2cfa7c7331b25ea101bf9531dc1f22e791d49
Ahmed Raza <ahmed.raza@square63.com>
2023-07-06 17:05:08 +0500

    Create profile and profile edit urls

--- pillreminder/urls.py
+++ pillreminder/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('accounts/signup/', views.SignUp.as_view(), name='signup')
+    path('accounts/signup/', views.SignUp.as_view(), name='signup'),
+    path('profile', views.profile, name='profile'),
+    path('profile/<pk>/edit', views.ProfileEdit.as_view(), name='profile-edit')
 ]
