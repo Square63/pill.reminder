22fc8924f7e2ca4128286e646180b09da885acd8
Ahmed Raza <ahmed.raza@square63.com>
2023-07-06 13:10:10 +0500

    Create a signup url

--- pillreminder/urls.py
+++ pillreminder/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('accounts/signup/', views.SignUp.as_view(), name='signup')
 ]
