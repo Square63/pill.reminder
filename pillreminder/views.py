27a93fdfc6ae01a02ee7b457903b1de2b9a139ba
Ahmed Raza <ahmed.raza@square63.com>
2023-07-06 17:05:28 +0500

    Views for profile and profile edit form

--- pillreminder/views.py
+++ pillreminder/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
-from .forms import SignUpForm
+from django.views.generic.edit import CreateView, UpdateView
+from .forms import SignUpForm, UserUpdateForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def home(request):
@@ -13,3 +15,16 @@ class SignUp(CreateView):
     template_name = 'pillreminder/signup.html'
     success_url = reverse_lazy("login")
     form_class = SignUpForm
+
+@login_required
+def profile(request):
+    return render(request, 'pillreminder/profile.html')
+
+class ProfileEdit(LoginRequiredMixin, UpdateView):
+    model = User
+    template_name = 'pillreminder/profile-edit.html'
+    success_url = reverse_lazy("profile")
+    form_class = UserUpdateForm
+
+    def get_object(self):
+        return self.request.user
