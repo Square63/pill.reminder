196ae51f7abd4d33e9025320b95ea381da75d4c4
Ahmed Raza <ahmed.raza@square63.com>
2023-07-06 17:04:11 +0500

    User profile update form

--- pillreminder/forms.py
+++ pillreminder/forms.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'password1', 'password2')
+
+class UserUpdateForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
