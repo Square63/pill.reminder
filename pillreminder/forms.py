6972d332560a2a31a0a4d8d16ae2aac034c821f8
Ahmed Raza <ahmed.raza@square63.com>
2023-07-07 18:16:08 +0500

    Add reminder form definition

--- pillreminder/forms.py
+++ pillreminder/forms.py
@@ -1,5 +1,9 @@
+from typing import Any
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+from django.forms.fields import Field
+from .models import Reminder, DAYS_CHOICES
 
 class SignUpForm(UserCreationForm):
     class Meta:
@@ -10,3 +14,16 @@ class UserUpdateForm(UserChangeForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
+
+class AddReminderForm(forms.ModelForm):
+    days = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=DAYS_CHOICES)
+    hours = forms.CharField(label='Hour', widget=forms.NumberInput(attrs={'min':1, 'max':12}))
+    minutes = forms.CharField(label='Minutes', widget=forms.NumberInput(attrs={'min':1, 'max':59}))
+    ampm = forms.ChoiceField(label=' ', choices=(('am', 'AM'), ('pm', 'PM')), required=False)
+    class Meta:
+        model = Reminder
+        exclude = ("user", "time")
+    def clean_days(self):
+        return ', '.join(self.cleaned_data['days'])
+    def clean_time(self):
+        return self.cleaned_data['hours'] + ':' + self.cleaned_data['minutes'] + self.cleaned_data['ampm']
