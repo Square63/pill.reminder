7fbd750aa40d786bd52a750505f20997e2190661
Ahmed Raza <ahmed.raza@square63.com>
2023-07-07 18:15:00 +0500

    Create reminder model

--- pillreminder/models.py
+++ pillreminder/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+DAYS_CHOICES = [
+    ("Sunday", "Sunday"),
+    ("Monday", "Monday"),
+    ("Tuesday", "Tuesday"),
+    ("Wednesday", "Wednesday"),
+    ("Thursday", "Thursday"),
+    ("Friday", "Friday"),
+    ("Saturday", "Saturday"),
+]
+
+class Reminder(models.Model):
+    is_active = models.BooleanField(default=True)
+    days = models.CharField(max_length=255, default="Monday")
+    time = models.CharField(max_length=20)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def get_days(self):
+        return self.days.split(',')
