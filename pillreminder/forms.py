8e01f9ba75ebb28ba6acded648e5394c5915fcce
Ahmed Raza <ahmed.raza@square63.com>
2023-07-10 12:04:52 +0500

    EditReminderForm

--- pillreminder/forms.py
+++ pillreminder/forms.py
@@ -27,3 +27,24 @@ class AddReminderForm(forms.ModelForm):
         return ', '.join(self.cleaned_data['days'])
     def clean_time(self):
         return self.cleaned_data['hours'] + ':' + self.cleaned_data['minutes'] + self.cleaned_data['ampm']
+
+class EditReminderForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(EditReminderForm, self).__init__(*args, **kwargs)
+        reminder = kwargs.get('instance')
+        days = []
+        for day in reminder.get_days():
+            days.append(day.strip())
+        self.initial['days'] = days
+        self.fields['hours'] = forms.CharField(label='Hour', widget=forms.NumberInput(attrs={'min':1, 'max':12}), initial=reminder.get_hours)
+        self.fields['minutes'] = forms.CharField(label='Minutes', widget=forms.NumberInput(attrs={'min':1, 'max':59}), initial=reminder.get_minutes)
+        self.fields['ampm'] = forms.ChoiceField(label=' ', choices=(('am', 'AM'), ('pm', 'PM')), required=False, initial=reminder.get_ampm)
+
+    days = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=DAYS_CHOICES)
+    class Meta:
+        model = Reminder
+        exclude = ("user", "time")
+    def clean_days(self):
+        return ', '.join(self.cleaned_data['days'])
+    def clean_time(self):
+        return self.cleaned_data['hours'] + ':' + self.cleaned_data['minutes'] + self.cleaned_data['ampm']
