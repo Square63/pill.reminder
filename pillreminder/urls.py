fbebf9ce29ab9a5cca9bcc6019c616d2386fc33c
Ahmed Raza <ahmed.raza@square63.com>
2023-07-10 12:02:30 +0500

    Reminder edit url

--- pillreminder/urls.py
+++ pillreminder/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('profile', views.profile, name='profile'),
     path('profile/<pk>/edit', views.ProfileEdit.as_view(), name='profile-edit'),
     path('add-reminder', views.AddReminder.as_view(), name='add-reminder'),
+    path('reminder/<pk>', views.reminder_details, name='reminder'),
+    path('reminder/<pk>/edit', views.EditReminder.as_view(), name='reminder-edit'),
 ]
