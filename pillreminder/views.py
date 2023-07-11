ca93ab0a5e41fff01a783876b657e19ae4b5038a
Ahmed Raza <ahmed.raza@square63.com>
2023-07-11 14:43:40 +0500

    Reminder update functionality along with Medicine update

--- pillreminder/views.py
+++ pillreminder/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView
-from .forms import SignUpForm, UserUpdateForm, AddReminderForm, EditReminderForm, MedicineFormSet
+from .forms import SignUpForm, UserUpdateForm, AddReminderForm, EditReminderForm, EditMedicineForm, MedicineFormSet, EditMedicineFormSet
+from django.forms import formset_factory
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Reminder, DAYS_CHOICES, Medicine
@@ -80,9 +81,34 @@ class EditReminder(LoginRequiredMixin, UpdateView):
     template_name = 'pillreminder/edit-reminder.html'
     success_url = reverse_lazy("home")
     form_class = EditReminderForm
-    def form_valid(self, form: EditReminderForm):
-        reminder = form.save(commit=False)
-        reminder.user = self.request.user
-        reminder.time = form.clean_time()
-        reminder.save()
-        return super(EditReminder, self).form_valid(form)
+    def get(self, *args, **kwargs):
+        reminder = Reminder.objects.get(id=kwargs.pop('pk'))
+        medicines = []
+        for medicine in reminder.medicine_set.all():
+            medicines.append({ "id": medicine.id, "name": medicine.name, "dosage": medicine.dosage })
+        medicineFormset = formset_factory(EditMedicineForm, extra=0)
+        formset = medicineFormset(initial=medicines)
+        form = EditReminderForm(instance=reminder)
+        form.formset = formset
+        return self.render_to_response({'form': form})
+
+    def post(self, *args, **kwargs):
+        reminder = Reminder.objects.get(id=kwargs.pop('pk'))
+        form = EditReminderForm(self.request.POST)
+        form.formset = EditMedicineFormSet(self.request.POST)
+        if form.is_valid() and form.formset.is_valid():
+            reminder.is_active = form.cleaned_data['is_active']
+            reminder.days = form.cleaned_data['days']
+            reminder.time = form.clean_time()
+            reminder.user = self.request.user
+            reminder.save()
+            for med_sub in form.formset.cleaned_data:
+                medicine = Medicine.objects.get(id=med_sub['id'])
+                medicine.name = med_sub['name']
+                medicine.dosage = med_sub['dosage']
+                medicine.reminder = reminder
+                medicine.save()
+            messages.success(self.request, 'The reminder has been updated.')
+            return redirect(reverse_lazy('home'))
+        else:
+            return self.render_to_response({'form': form})
