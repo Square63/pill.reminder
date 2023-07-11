7c065dda398ee628be3dde5d13ac3a0a9c9924a2
Ahmed Raza <ahmed.raza@square63.com>
2023-07-11 14:42:20 +0500

    Reminder edit form along with edit medicine formset

--- pillreminder/forms.py
+++ pillreminder/forms.py
@@ -31,15 +31,20 @@ class EditReminderForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(EditReminderForm, self).__init__(*args, **kwargs)
         reminder = kwargs.get('instance')
+        if reminder is None:
+            return
         days = []
         for day in reminder.get_days():
             days.append(day.strip())
         self.initial['days'] = days
-        self.fields['hours'] = forms.CharField(label='Hour', widget=forms.NumberInput(attrs={'min':1, 'max':12}), initial=reminder.get_hours)
-        self.fields['minutes'] = forms.CharField(label='Minutes', widget=forms.NumberInput(attrs={'min':1, 'max':59}), initial=reminder.get_minutes)
-        self.fields['ampm'] = forms.ChoiceField(label=' ', choices=(('am', 'AM'), ('pm', 'PM')), required=False, initial=reminder.get_ampm)
+        self.initial['hours'] = reminder.get_hours
+        self.initial['minutes'] = reminder.get_minutes
+        self.initial['ampm'] = reminder.get_ampm
 
     days = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=DAYS_CHOICES)
+    hours = forms.CharField(label='Hour', widget=forms.NumberInput(attrs={'min':1, 'max':12}))
+    minutes = forms.CharField(label='Minutes', widget=forms.NumberInput(attrs={'min':1, 'max':59}))
+    ampm = forms.ChoiceField(label=' ', choices=(('am', 'AM'), ('pm', 'PM')), required=False)
     class Meta:
         model = Reminder
         exclude = ("user", "time")
@@ -55,11 +60,26 @@ class MedicineAddForm(forms.ModelForm):
         self.fields['dosage'].widget.attrs['max'] = 1000
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control'
+    class Meta:
+        model = Medicine
+        fields = ("name", "dosage")
 
+class EditMedicineForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['dosage'].widget.attrs['min'] = 1
+        self.fields['dosage'].widget.attrs['max'] = 1000
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+    id = forms.IntegerField(widget=forms.HiddenInput())
     class Meta:
         model = Medicine
         fields = ("name", "dosage")
 
 MedicineFormSet = formset_factory(
     MedicineAddForm, extra=1
+)
+
+EditMedicineFormSet = formset_factory(
+    EditMedicineForm, extra=1
 )
