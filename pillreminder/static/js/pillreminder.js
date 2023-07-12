99aacb7c80e16790a2ee6140d9a1f82506ad004e
Ahmed Raza <ahmed.raza@square63.com>
2023-07-12 16:42:36 +0500

    Remove the Medicine DOM on remove button click

--- pillreminder/static/js/pillreminder.js
+++ pillreminder/static/js/pillreminder.js
@@ -1,10 +1,12 @@
 let medicines = document.querySelectorAll(".medicines")
 let container = document.querySelector("#medicines")
 let addButton = document.querySelector("#add-form")
+let removeButton = document.querySelector("#remove-form")
 let totalForms = document.querySelector("#id_form-TOTAL_FORMS")
 
 let formNum = medicines.length-1
 addButton.addEventListener('click', addForm)
+removeButton.addEventListener('click', removeForm)
 
 function addForm(e) {
   e.preventDefault()
@@ -17,4 +19,13 @@ function addForm(e) {
   container.insertBefore(newForm, addButton)
 
   totalForms.setAttribute('value', `${formNum+1}`)
+}
+
+function removeForm(e) {
+  e.preventDefault()
+  let allMedicines = document.querySelectorAll(".medicines")
+  let indexToRemove = allMedicines.length - 1
+  allMedicines[0].parentElement.removeChild(allMedicines[indexToRemove])
+  formNum++
+  totalForms.setAttribute('value', `${formNum-1}`)
 }
