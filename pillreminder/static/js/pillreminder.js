11f1b87e898071cd8aa88a71b46f7149e927bcf1
Ahmed Raza <ahmed.raza@square63.com>
2023-07-17 11:42:31 +0500

    Change to add/remove medicine functionality, individual remove button for each medicine

--- pillreminder/static/js/pillreminder.js
+++ pillreminder/static/js/pillreminder.js
@@ -1,12 +1,23 @@
 let medicines = document.querySelectorAll(".medicines")
 let container = document.querySelector("#medicines")
 let addButton = document.querySelector("#add-form")
-let removeButton = document.querySelector("#remove-form")
+let removeButton = document.querySelectorAll(".remove-form")
 let totalForms = document.querySelector("#id_form-TOTAL_FORMS")
 
 let formNum = medicines.length-1
 addButton.addEventListener('click', addForm)
-removeButton.addEventListener('click', removeForm)
+
+function removeButtonEventListener(disabled = false) {
+  removeButton.forEach(function(button){
+    button.addEventListener('click', removeForm)
+    button.setAttribute('disabled', true);
+    if (disabled == false) {
+      button.removeAttribute('disabled');
+    }
+  })
+}
+
+removeButtonEventListener(disabled = formNum == 0)
 
 function addForm(e) {
   e.preventDefault()
@@ -15,24 +26,34 @@ function addForm(e) {
   let formRegex = RegExp(`form-(\\d){1}-`,'g')
 
   formNum++
-  removeButton.removeAttribute('disabled')
+
   newForm.innerHTML = newForm.innerHTML.replace(formRegex, `form-${formNum}-`)
   container.insertBefore(newForm, addButton)
-
+  if (newForm.querySelector(`input[name=form-${formNum}-id]`) != null) {
+    newForm.querySelector(`input[name=form-${formNum}-id]`).value = 0
+  }
+  newForm.querySelector('input[type=text]').value = ''
+  newForm.querySelector('input[type=number]').value = ''
   totalForms.setAttribute('value', `${formNum+1}`)
+  removeButton = document.querySelectorAll(".remove-form")
+  if (formNum) {
+    removeButtonEventListener(disabled = false);
+  }
 }
 
 function removeForm(e) {
   e.preventDefault()
-  let allMedicines = document.querySelectorAll(".medicines")
-  let indexToRemove = allMedicines.length - 1
-  if (indexToRemove === 1) {
-    removeButton.setAttribute('disabled', true)
+
+  let elSelector = e.target.getAttribute('data-id')
+  let medicine = document.getElementsByClassName(elSelector)
+  let deleteButton = document.getElementById(elSelector.replace('-name', '-delete'));
+  medicine[0].style.display = 'none'
+  deleteButton.value = 1
+
+  formNum = formNum-1
+
+  if (formNum == 0) {
+    let firstButton = document.querySelector(`button[data-id=id_form-${formNum}-name]`)
+    firstButton.setAttribute('disabled', true);
   }
-  if (indexToRemove === 0) {
-    return;
-  }
-  allMedicines[0].parentElement.removeChild(allMedicines[indexToRemove])
-  formNum++
-  totalForms.setAttribute('value', `${formNum-1}`)
 }
