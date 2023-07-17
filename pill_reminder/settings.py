2aa21dfe62af50a2bdb67812e9a9f0c00907a6f9
Ahmed Raza <ahmed.raza@square63.com>
2023-07-17 18:24:51 +0500

    Configurations for file uploads

--- pill_reminder/settings.py
+++ pill_reminder/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,6 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 #EMAIL_HOST='localhost'
 #EMAIL_PORT=1025
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
