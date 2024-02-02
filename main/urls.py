b93cd6bc88d340aca863d26a23ef85300710e5c0
Ahmed Raza <ahmed.raza@square63.com>
2024-02-02 14:25:31 +0500

    Register login and signup api endpoints

--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
-from . import views
+from main.views import (
+    register_view,
+    login_view
+)
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('accounts/signup/', views.SignUp.as_view(), name='signup'),
-    path('profile', views.profile, name='profile'),
-    path('profile/<pk>/edit', views.ProfileEdit.as_view(), name='profile-edit'),
-    path('add-reminder', views.AddReminder.as_view(), name='add-reminder'),
-    path('reminder/<pk>', views.reminder_details, name='reminder'),
-    path('reminder/<pk>/edit', views.EditReminder.as_view(), name='reminder-edit'),
-    path('reminder/<pk>/delete', views.DeleteReminder.as_view(), name='reminder-delete'),
-    path('contact', views.contact, name='contact'),
+    path('auth/signup', register_view, name='signup'),
+    path('auth/login', login_view, name='token_obtain_pair'),
+    # path('profile', views.profile, name='profile'),
+    # path('profile/<pk>/edit', views.ProfileEdit.as_view(), name='profile-edit'),
+    # path('add-reminder', views.AddReminder.as_view(), name='add-reminder'),
+    # path('reminder/<pk>', views.reminder_details, name='reminder'),
+    # path('reminder/<pk>/edit', views.EditReminder.as_view(), name='reminder-edit'),
+    # path('reminder/<pk>/delete', views.DeleteReminder.as_view(), name='reminder-delete'),
+    # path('contact', views.contact, name='contact'),
 ]
