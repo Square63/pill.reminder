5b29f5639520e9cd38590238e698a9a9aead6768
Ahmed Raza <ahmed.raza@square63.com>
2024-02-02 17:39:06 +0500

    Sigin and Reminders endpoints

--- main/urls.py
+++ main/urls.py
@@ -1,17 +1,12 @@
 from django.urls import path
 from main.views import (
     register_view,
-    login_view
+    login_view,
+    reminders_view
 )
 
 urlpatterns = [
     path('auth/signup', register_view, name='signup'),
-    path('auth/login', login_view, name='token_obtain_pair'),
-    # path('profile', views.profile, name='profile'),
-    # path('profile/<pk>/edit', views.ProfileEdit.as_view(), name='profile-edit'),
-    # path('add-reminder', views.AddReminder.as_view(), name='add-reminder'),
-    # path('reminder/<pk>', views.reminder_details, name='reminder'),
-    # path('reminder/<pk>/edit', views.EditReminder.as_view(), name='reminder-edit'),
-    # path('reminder/<pk>/delete', views.DeleteReminder.as_view(), name='reminder-delete'),
-    # path('contact', views.contact, name='contact'),
+    path('auth/signin', login_view, name='token_obtain_pair'),
+    path('reminders', reminders_view, name='user_reminders')
 ]
