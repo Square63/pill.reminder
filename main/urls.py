8bcbc119c8e2dec9b80ae49e7ac827931fdb50c8
Ahmed Raza <ahmed.raza@square63.com>
2024-02-06 12:10:02 +0500

    Token, refresh token and verify token endpoints

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 from main.views import (
     register_view,
     login_view,
@@ -8,5 +15,8 @@ from main.views import (
 urlpatterns = [
     path('auth/signup', register_view, name='signup'),
     path('auth/signin', login_view, name='token_obtain_pair'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('reminders', reminders_view, name='user_reminders')
 ]
