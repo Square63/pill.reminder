920bb7f278e79e2f42a8341f875fe6ead2fe0346
Ahmed Raza <ahmed.raza@square63.com>
2024-02-09 19:11:56 +0500

    Password and email validation

--- main/serializers/UserSerializer.py
+++ main/serializers/UserSerializer.py
@@ -1,18 +1,34 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core.validators import EmailValidator
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     full_name = serializers.SerializerMethodField('_get_full_name')
+    email = serializers.EmailField(required=True, validators=[EmailValidator])
+    password = serializers.CharField(write_only=True, required=False, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=False)
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('id', 'first_name', 'last_name', 'full_name', 'email', 'password', 'password2', 'last_login')
+    def validate(self, attrs):
+        password = attrs.get('password', None)
+        password2 = attrs.pop('password2', None)
+        if (password is not None or password2 is not None) and (password != password2):
+            raise serializers.ValidationError("Password and Confirm Password does not match.")
+
+        return attrs
+    def validate_email(self, value):
+        try:
+            user = User.objects.get(email=value)
+        except User.DoesNotExist:
+            user = None
+        if user is not None and user.id != self.context.id:
+            raise serializers.ValidationError("Email already exists.")
+        else:
+            return value
     def to_representation(self, instance):
         data = super(UserSerializer, self).to_representation(instance)
-        data.pop('is_superuser')
-        data.pop('groups')
-        data.pop('user_permissions')
-        data.pop('password')
-        data.pop('is_staff')
         return data
     def _get_full_name(self, user_object):
         return user_object.get_full_name()
