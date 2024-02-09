bcd71663ee699cd64db11448ff675f0883ce4af0
Ahmed Raza <ahmed.raza@square63.com>
2024-02-09 14:53:12 +0500

    Reminder view

---
+++ main/views/ReminderView.py
@@ -0,0 +1,22 @@
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from ..serializers.ReminderSerializer import ReminderSerializer
+from ..models import Reminder
+
+class ReminderView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ReminderSerializer
+    def get(self, request, pk=None):
+        try:
+            reminder = request.user.reminder_set.get(pk=pk)
+        except Reminder.DoesNotExist:
+            reminder = None
+
+        if reminder is not None:
+            serializer = self.serializer_class(request.user.reminder_set.get(pk=pk))
+            return Response(serializer.data)
+        else:
+            return Response({'message': 'Reminder not found'}, status=status.HTTP_404_NOT_FOUND)
+
+reminder_view = ReminderView.as_view()
