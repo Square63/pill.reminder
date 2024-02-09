72688d14ee32b573f0632355bdc48fbb4d511402
Ahmed Raza <ahmed.raza@square63.com>
2024-02-09 14:52:26 +0500

    Register Reminder and User view

--- main/views/__init__.py
+++ main/views/__init__.py
@@ -1,9 +1,13 @@
 from main.views.RegisterView import register_view
 from main.views.LoginView import login_view
 from main.views.RemindersView import reminders_view
+from main.views.ReminderView import reminder_view
+from main.views.UserView import user_view
 
 __all__ = [
     "register_view",
     "login_view",
-    "reminders_view"
+    "reminders_view",
+    "reminder_view",
+    "user_view"
 ]
