b808c2ba4d91d6a97a6b13bd95cd086d406238e6
Ahmed Raza <ahmed.raza@square63.com>
2024-02-09 14:40:32 +0500

    Send medicines data along with reminder object

--- main/serializers/ReminderSerializer.py
+++ main/serializers/ReminderSerializer.py
@@ -1,14 +1,19 @@
 from rest_framework import serializers
 from ..models import Reminder
+from .MedicineSerializer import MedicineSerializer
 
 class ReminderSerializer(serializers.ModelSerializer):
-    days = serializers.SerializerMethodField('days_info')
+    days = serializers.SerializerMethodField('_days_info')
+    medicines = serializers.SerializerMethodField('_get_medicines')
     class Meta:
         model = Reminder
         fields = '__all__'
     def to_representation(self, instance):
         data =  super(ReminderSerializer, self).to_representation(instance)
-        # data.pop('time')
         return data
-    def days_info(self, obj):
-        return obj.get_selected_days()
+    def _days_info(self, resume_object):
+        return resume_object.get_selected_days()
+    def _get_medicines(self, resume_object):
+        medicines = resume_object.medicine_set.all()
+        medicines_serializer = MedicineSerializer(medicines, many=True)
+        return medicines_serializer.data
