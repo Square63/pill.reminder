d71680e17cfdbfc50902c79124f605c2de481f38
Alex Roye <alex@creativefaze.com>
2024-02-12 19:23:52 +0500

    Get the dosage customized value in to_representation instead

--- main/serializers/MedicineSerializer.py
+++ main/serializers/MedicineSerializer.py
@@ -3,12 +3,10 @@ from ..models import Medicine
 import json
 
 class MedicineSerializer(serializers.ModelSerializer):
-  dosage = serializers.SerializerMethodField('_get_dosage')
   class Meta:
     model = Medicine
     fields = '__all__'
   def to_representation(self, instance):
     data =  super(MedicineSerializer, self).to_representation(instance)
+    data['dosage'] = instance.get_dosage()
     return data
-  def _get_dosage(self, medicine_object):
-    return medicine_object.get_dosage()
