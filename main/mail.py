6023d1ba18ea48be2a09bdca798966688141c2e2
Alex Roye <alex@creativefaze.com>
2024-02-15 17:29:50 +0500

    Pass on user obj to reminder email template to render user full name

--- main/mail.py
+++ main/mail.py
@@ -5,7 +5,7 @@ def send_reminder_email(user, reminders):
     subject = 'Pill Reminder'
     message = '<p>This is a reminder.</p>'
     htmly = get_template('main/emails/reminder.html')
-    d = { 'reminders': reminders }
+    d = { 'user': user, 'reminders': reminders }
     html_content = htmly.render(d)
     msg = EmailMultiAlternatives(subject, message, "main@square63.com", [user.email])
     msg.attach_alternative(html_content, "text/html")
