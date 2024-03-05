1b7e2e077000707a2de644de2d42eb833b7e2861
Ahmed Raza <ahmed.raza@square63.com>
2024-03-05 12:49:52 +0500

    Return all family reminders

--- main/views/RemindersView.py
+++ main/views/RemindersView.py
@@ -7,7 +7,7 @@ class RemindersView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = ReminderSerializer
     def get(self, request, *arg, **kwargs):
-        serializer = self.serializer_class(request.user.reminder_set.all(), many=True)
+        serializer = self.serializer_class(request.user.userprofile.family.reminders.all(), many=True)
         return Response(serializer.data)
 
 reminders_view = RemindersView.as_view()
