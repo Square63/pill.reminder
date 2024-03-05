b59896e06040cb76bad363d994e0198398fe6836
Ahmed Raza <ahmed.raza@square63.com>
2024-03-05 14:45:40 +0500

    Find reminder by id

--- main/views/ReminderView.py
+++ main/views/ReminderView.py
@@ -9,12 +9,12 @@ class ReminderView(generics.GenericAPIView):
     serializer_class = ReminderSerializer
     def get(self, request, pk=None):
         try:
-            reminder = request.user.reminder_set.get(pk=pk)
+            reminder = Reminder.objects.get(pk=pk)
         except Reminder.DoesNotExist:
             reminder = None
 
         if reminder is not None:
-            serializer = self.serializer_class(request.user.reminder_set.get(pk=pk))
+            serializer = self.serializer_class(reminder)
             return Response(serializer.data)
         else:
             return Response({'message': 'Reminder not found'}, status=status.HTTP_404_NOT_FOUND)
