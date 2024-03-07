2bd0c8e60d72011772c2b135e335848d95f71578
Ahmed Raza <ahmed.raza@square63.com>
2024-03-07 19:03:12 +0500

    Send time object and type human readable value

--- main/serializers/ReminderTypeSerializer.py
+++ main/serializers/ReminderTypeSerializer.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from ..models import ReminderType
+from datetime import datetime
+from ..models import ReminderType, REMINDER_CHOICES
 
 class ReminderTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReminderType
         fields = '__all__'
+    def to_representation(self, instance):
+        data =  super(ReminderTypeSerializer, self).to_representation(instance)
+        data['object'] = datetime.strptime(instance.time, '%H:%M')
+        reminder_choices  = {key: value for key, value in REMINDER_CHOICES}
+        data['type_normal'] = reminder_choices.get(instance.type)
+        return data
