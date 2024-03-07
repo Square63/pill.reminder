8ab4cd3779a37b1c7f5dec2fbd8efbacaf945b94
Ahmed Raza <ahmed.raza@square63.com>
2024-03-07 18:27:08 +0500

    Remove all the time related properties from reminder serializer

--- main/serializers/ReminderSerializer.py
+++ main/serializers/ReminderSerializer.py
@@ -7,8 +7,6 @@ from .UserSerializer import UserSerializer
 import json
 
 class ReminderSerializer(serializers.ModelSerializer):
-    hours = serializers.SerializerMethodField('_get_hours')
-    minutes = serializers.SerializerMethodField('_get_minutes')
     class Meta:
         model = Reminder
         fields = '__all__'
@@ -16,20 +14,12 @@ class ReminderSerializer(serializers.ModelSerializer):
         data =  super(ReminderSerializer, self).to_representation(instance)
         medicines = instance.medicine_set.all()
         medicines = MedicineSerializer(medicines, many=True)
-        reminder_type = ReminderTypeSerializer(instance.get_reminder_type())
-        reminder_type = reminder_type.data['reminder_type'].capitalize() if reminder_type.data['reminder_type'] is not None else None
+        times = ReminderTypeSerializer(instance.remindertype_set.all(), many=True)
         data['medicines'] = medicines.data
         data['days'] = instance.get_selected_days()
         data['days_arr'] = instance.get_stripped_days()
-        data['ampm'] = instance.get_ampm()
-        data['hours'] = instance.get_hours()
-        data['time'] = instance.get_time()
-        data['type'] = reminder_type
+        data['times'] = times.data
         data['user'] = UserSerializer(instance.user).data
         data['created_at'] = instance.created_at.strftime('%a, %d %b %Y %H:%M:%S')
         data['updated_at'] = instance.updated_at.strftime('%a, %d %b %Y %H:%M:%S')
         return data
-    def _get_hours(self, reminder_object):
-        return reminder_object.get_hours()
-    def _get_minutes(self, reminder_object):
-        return reminder_object.get_minutes()
