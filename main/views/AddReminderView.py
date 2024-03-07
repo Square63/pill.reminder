aa9f392a3b28113ff08aba0f0e84f0fa0e5f97e1
Ahmed Raza <ahmed.raza@square63.com>
2024-03-07 19:03:57 +0500

    Store time in reminderType model

--- main/views/AddReminderView.py
+++ main/views/AddReminderView.py
@@ -10,34 +10,19 @@ import json
 class AddReminderView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = ReminderSerializer
-    def get_time(self):
-        hours = self.request.data.get('hours')
-        minutes = self.request.data.get('minutes')
-        if int(hours) < 10 and len(hours) == 1:
-            hours = '0'+str(hours)
-        if int(minutes) < 10 and len(minutes) == 1:
-            minutes = '0'+str(minutes)
-        time_12_format = hours+':'+minutes+self.request.data.get('ampm')
-        return datetime.strptime(time_12_format, '%I:%M%p').time()
 
     def post(self, request, *arg, **kwargs):
         form_data = self.request.data
         medicines = form_data.get('medicines')
         form_data['family'] = request.user.userprofile.family.id
         errors = {}
-        if form_data.get('hours') == '' or form_data.get('hours') is None:
-            errors['hours'] = ['This field is required.']
-        if form_data.get('minutes') == '' or form_data.get('minutes') is None:
-            errors['minutes'] = ['This field is required.']
-        if form_data.get('ampm') == '' or form_data.get('ampm') is None:
-            errors['ampm'] = ['This field is required.']
         if form_data.get('days') == '' or form_data.get('days') is None:
             errors['days'] = ['This field is required.']
         if medicines is None:
             errors['non_field_errors'] = ['Atleast add one medicine.']
         if errors:
             return Response(errors, status=status.HTTP_400_BAD_REQUEST)
-        form_data['time'] = str(self.get_time())
+
         form_data['days'] = ', '.join(form_data['days'])
         reminder_serializer = self.serializer_class(data=form_data)
         if reminder_serializer.is_valid():
@@ -46,17 +31,18 @@ class AddReminderView(generics.GenericAPIView):
             return Response(reminder_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         form_data['reminder'] = instance.id
-        reminder_type_serializer = ReminderTypeSerializer(data=form_data)
-        if reminder_type_serializer.is_valid():
-            reminder_type_serializer.save()
-        else:
-            return Response(reminder_type_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+        times = form_data.get('times')
+        times = json.loads(times)
+        for time in times:
+            time.pop('object')
+            time['reminder'] = instance.id
+        reminder_type_serializer = ReminderTypeSerializer(data=times, many=True)
+        reminder_type_serializer.is_valid(raise_exception=True)
+        reminder_type_serializer.save()
         medicines = form_data.get('medicines')
         medicines = json.loads(medicines)
         for medicine in medicines:
             medicine['reminder'] = instance.id
-            medicine['dosage'] = int(medicine['dosage'])
         medicines_serializer = MedicineSerializer(data=medicines, many=True)
         medicines_serializer.is_valid(raise_exception=True)
         medicines_serializer.save()
