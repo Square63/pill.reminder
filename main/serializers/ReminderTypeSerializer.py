152c25609089efad6c6db7463b0cefcfdeed26f4
Ahmed Raza <ahmed.raza@square63.com>
2024-03-12 00:49:15 +0500

    Remove empty space

--- main/serializers/ReminderTypeSerializer.py
+++ main/serializers/ReminderTypeSerializer.py
@@ -7,7 +7,7 @@ class ReminderTypeSerializer(serializers.ModelSerializer):
         model = ReminderType
         fields = '__all__'
     def to_representation(self, instance):
-        data =  super(ReminderTypeSerializer, self).to_representation(instance)
+        data = super(ReminderTypeSerializer, self).to_representation(instance)
         data['object'] = datetime.strptime(instance.time, '%H:%M')
         reminder_choices  = {key: value for key, value in REMINDER_CHOICES}
         data['type_normal'] = reminder_choices.get(instance.type)
