ca8f25f53be9e065110f1006a2df28a9d6aa0505
Ahmed Raza <ahmed.raza@square63.com>
2024-03-11 23:58:27 +0500

    Send the remaining time information with Phone object

--- main/serializers/PhoneSerializer.py
+++ main/serializers/PhoneSerializer.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from ..models import Phone
+from datetime import datetime
 
 class PhoneSerializer(serializers.ModelSerializer):
     class Meta:
         model = Phone
         fields = '__all__'
+    def to_representation(self, instance):
+        data = super(PhoneSerializer, self).to_representation(instance)
+        data['resend_in'] = round(instance.sent_on.timestamp()) - round(datetime.now().timestamp())
+        return data
