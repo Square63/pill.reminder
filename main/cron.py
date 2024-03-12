5128077d30f67f8952712addcab470825e5d15c4
Ahmed Raza <ahmed.raza@square63.com>
2024-03-12 14:16:03 +0500

    Fixes to email reminder cron

--- main/cron.py
+++ main/cron.py
@@ -1,9 +1,17 @@
 from .models import UserMethods as User
 from .mail import send_reminder_email
+from datetime import datetime, timedelta
 
 def send_reminder():
     users = User.objects.all()
     for user in users:
         reminders = user.next_reminders()
         if reminders:
-            send_reminder_email(user, reminders)
+            for reminder in reminders:
+                hour = datetime.now().strftime('%H:%M:%S')
+                one_hour_later = datetime.now() + timedelta(hours=1)
+                one_hour_later = one_hour_later.strftime('%H:%M:%S')
+                remindertypes = reminder.remindertype_set.filter(time__range=(hour, one_hour_later))
+                for remindertype in remindertypes:
+                    if remindertype.type == 'email':
+                        send_reminder_email(user, reminder, remindertype)
--- main/mail.py
+++ main/mail.py
@@ -1,13 +1,13 @@
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 
-def send_reminder_email(user, reminders):
+def send_reminder_email(user, reminder, remindertype):
     subject = 'Pills Reminder'
     message = '<p>This is a reminder.</p>'
     htmly = get_template('main/emails/reminder.html')
-    d = { 'user': user, 'reminders': reminders }
+    d = { 'user': user, 'reminder': reminder, 'remindertype': remindertype }
     html_content = htmly.render(d)
     msg = EmailMultiAlternatives(subject, message, "Pills Reminder <main@square63.com>", [user.email])
     msg.attach_alternative(html_content, "text/html")
     msg.send()
-    print('Reminder was sent.')
+    print(f"Reminder was sent to {user.email}.")
